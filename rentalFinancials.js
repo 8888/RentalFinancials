bfb4306a39037bc23d4162e77aa094f5fdbf98f4
betterin30days <leecostello1@gmail.com>
2016-11-15 21:44:00 -0500

    Initial outline for the project

---
+++ rentalFinancials.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class NPV {
+    static FV(rate, periods, payment) {
+        let fv = "4500";
+        return fv;
+    }
+
+    static PV(rate, periods, futureValue) {
+        let pv = 0;
+        return pv;
+    }
+
+    static PMT(rate, periods, presentValue) {
+        let pmt = 0;
+        return pmt;
+    }
+
+    static IIR(payments, precision = 0.001) {
+        let iir = 0;
+        return iir;
+    }
+}
