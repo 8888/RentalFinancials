63ac38ba7518dd2dfda42c5e7bf3070530fb49c7
betterin30days <leecostello1@gmail.com>
2016-11-17 20:31:56 -0500

    Created future value formula

--- rentalFinancials.js
+++ rentalFinancials.js
@@ -1,9 +1,22 @@
 'use strict';
 
+function rnd(base, percision) {
+    // round a number to the giving amount of decimal places
+    let x = Math.pow(10, percision);
+    let result = Math.round(base * x) / x;
+    return result;
+}
+
 class NPV {
     static FV(rate, periods, payment) {
-        let fv = "4500";
-        return fv;
+        // Calculates the future value of cashflow
+        if (rate != 0) {
+            let rateDecimal = rate / 100;
+            var fv = (payment * ((Math.pow(1 + rateDecimal, periods) - 1) / rateDecimal));
+        } else {
+            var fv = payment * periods;
+        }
+        return rnd(fv, 3);
     }
 
     static PV(rate, periods, futureValue) {
